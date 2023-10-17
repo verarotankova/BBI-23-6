9f5a10dd98363bc01133e205abd6ae236489cd7c
verarotankova <146060504+verarotankova@users.noreply.github.com>
2023-10-17 19:50:27 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -326,6 +326,51 @@ namespace ConsoleApp1
                     S += K;
                    
                     Console.WriteLine($"x = {X}, s = {S}, y = {Math.Cos(X)}");
+
+            double s = 0; 
+            double a = 0.1, b = 1, h = 0.1;
+            double y; 
+            double eps = 0.0001; 
+            double now_elem; 
+            double fact;
+            double stp = 1;
+            for (double x = a; x <= b; x += h)
+            {
+                y = Math.Cos(x);
+                int i = 0;
+                while (true)
+                {
+                   fact = 1;
+                   stp = 1;
+                   for (int j = 1; j <= 2 * i; j++)
+                   {
+                       fact *= j;
+                       stp *= x;
+                   }
+                   if (i % 2 == 0)
+                   {
+                       now_elem = stp / fact;
+                   }
+                   else
+                   {
+                       now_elem = -stp / fact;
+                   }
+
+                   if (Math.Abs(now_elem) < eps)
+                   {
+                        break;
+                   }
+                   else
+                   {
+                        s += now_elem;
+                   }
+                   i++;
+                }
+                Console.WriteLine(s + " " + y);
+                Console.WriteLine();
+                s = 0;
+            }
+            Console.ReadKey();
                 }
             }
         }
